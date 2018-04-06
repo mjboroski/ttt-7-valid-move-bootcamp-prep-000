3ab8bbdc39a79962345e2cdd05f6ffb465fad0fd
Michael Boroski <mjboroski@gmail.com>
2018-04-06 23:51:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,12 @@
 def valid_move?(board, index)
-  if position_taken?
+  if position_taken?(board, index)
     return false
   else
     return true
   end
 end
 
-def position_taken?
+def position_taken?(board, index)
   if board[index]=="X" || board[index]=="O"
     return true
   else
